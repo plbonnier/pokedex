6802344fa3cf80a1ba543991be4007894629c927
plbonnier <pl_bonnier@yahoo.fr>
2024-10-23 10:01:20 +0200

    quete 3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,19 @@
+const pokemonList = [
+    {
+        name: 'Bulbasaur',
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    let pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>} 
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
